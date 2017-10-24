7d44798a0e3db3c7bc2684f634d8d2e5dfaf4891
mtso <mtso@tuta.io>
2017-10-24 14:27:26 -0700

    Configure cmake to build multiple header files in

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@
 #include <cpr/cpr.h>
 #include <json.hpp>
 
+#include "meaning.h"
+
 int main(int argc, char** argv) {
     std::string json_url = "https://jsonbin.io/b/59eeea97ed25d0573bf19888";
 
     auto response = cpr::Get(cpr::Url{json_url});
     auto json = nlohmann::json::parse(response.text);
     auto list = json["list"];
-    
+
     for (int i = 0; i < list.size(); i++) {
         std::cout << list[i] << std::endl;
     }
+
+    std::cout << bj::get_meaning() << std::endl;
 }
